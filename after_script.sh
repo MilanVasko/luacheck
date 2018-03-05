89056ea8204b431b23f1a9d57d489ab82ed93fd8
Milan Vaško <m.vasko2@gmail.com>
2018-03-05 21:34:36 +0100

    Trying to push the resulting md file to github pages

--- after_script.sh
+++ after_script.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
-pkg_dir="$PWD"
-report_file="$pkg_dir/../_luadist_install/*.md"
+set -xe
 
-echo $report_file
+pkg_dir="$PWD"
+pkg_name="`basename $pkg_dir`"
+report_file="$pkg_dir/../_luadist_install/*.md"
+cloned_repo="$pkg_dir/../_luadist_packages_web"
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git clone https://github.com/MilanVasko/LuaDist2-Packages $cloned_repo
+cp $report_file $cloned_repo
+cd $cloned_repo
+git add --all
+git commit -m "${pkg_name} md file"
+git remote add origin_key https://${GITHUB_ACCESS_TOKEN}@github.com/MilanVasko/LuaDist2-Packages
+git push origin_key master
 
