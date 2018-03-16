3e32946ca908d49a25eb7d38773fa9dea325577a
Milan Vaško <m.vasko2@gmail.com>
2018-03-16 20:17:15 +0100

    Tried to update the after script to work with new github pages layour

--- after_script.sh
+++ after_script.sh
@@ -10,8 +10,18 @@ cloned_repo="$pkg_dir/../_luadist_packages_web"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git clone https://github.com/MilanVasko/LuaDist2-Packages $cloned_repo
-cp $report_file $cloned_repo
 cd $cloned_repo
+cd packages
+mkdir -p $pkg_name
+cd $pkg_name
+
+echo "---" > index.md
+echo "package_name: ${$pkg_name}" >> index.md
+echo "pages:" >> index.md
+echo "    - install" >> index.md
+echo "---" >> index.md
+
+cp $report_file ./install.md
 git add --all
 git commit -m "${pkg_name} md file"
 git remote add origin_key https://${GITHUB_ACCESS_TOKEN}@github.com/MilanVasko/LuaDist2-Packages
